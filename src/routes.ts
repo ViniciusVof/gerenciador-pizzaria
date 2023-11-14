7fa83b3dc47cb58283596a1eb9bc8246c8334d70
Vinicius Freitas <viniciusfreitas@MacBook-Pro-de-MacInBox.local>
2023-11-14 18:12:47 -0300

    Cadastro de produtos e upload de imagens

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
+import multer from "multer";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ReadCategoryController } from "./controllers/category/ListCategoryController";
+import { CreateProductController } from "./controllers/product/CreateProductController";
+
+import uploadConfig from "./config/multer";
 
 const router = Router();
+
+const upload = multer(uploadConfig.upload("./tmp"));
 
 router.post("/user", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
@@ -18,5 +24,12 @@ router.post(
   new CreateCategoryController().handle
 );
 router.get("/categories", isAuthenticated, new ReadCategoryController().handle);
+
+router.post(
+  "/product",
+  isAuthenticated,
+  upload.single("file"),
+  new CreateProductController().handle
+);
 
 export { router };
