d37f05e089fe2d80009281aa6b507d465dcc7769
Vinicius Freitas <viniciusfreitas@MacBook-Pro-de-MacInBox.local>
2023-11-14 13:20:13 -0300

    Validação de erros no cadastro de usuário

--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,3 +1,4 @@
+import prismaClient from "../../prisma";
 interface UserRequest {
   name: string;
   email: string;
@@ -5,7 +6,34 @@ interface UserRequest {
 }
 class CreateUserService {
   async execute({ name, email, password }: UserRequest) {
-    return { name, email, password };
+    if (!email) {
+      throw new Error("Email incorrect");
+    }
+
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const user = await prismaClient.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
+    });
+
+    return user;
   }
 }
 
