4b976c6cc27228b9f05de7c80a1d859f56ec82f0
Vinicius Freitas <viniciusfreitas@MacBook-Pro-de-MacInBox.local>
2023-11-14 16:39:30 -0300

    Controller e Service de cadastro de categorias

---
+++ src/services/category/CreateCategoryService.ts
@@ -0,0 +1,36 @@
+import prismaClient from "../../prisma";
+import { hash } from "bcryptjs";
+interface CategoryRequest {
+  name: string;
+}
+class CreateCategoryService {
+  async execute({ name }: CategoryRequest) {
+    if (!name) {
+      throw new Error("Name incorrect");
+    }
+
+    const categoryAlreadyExists = await prismaClient.category.findFirst({
+      where: {
+        name,
+      },
+    });
+
+    if (categoryAlreadyExists) {
+      throw new Error("Category already exists");
+    }
+
+    const category = await prismaClient.category.create({
+      data: {
+        name,
+      },
+      select: {
+        id: true,
+        name: true,
+      },
+    });
+
+    return category;
+  }
+}
+
+export { CreateCategoryService };
