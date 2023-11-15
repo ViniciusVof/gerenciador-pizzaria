66c2bf8f4dc8e9ec890042a51edef8ffd524c245
Vinicius Freitas <viniciusfreitas@MacBook-Pro-de-MacInBox.local>
2023-11-14 23:09:16 -0300

    Envio do pedido

--- src/routes.ts
+++ src/routes.ts
@@ -14,6 +14,7 @@ import { CreateOrderController } from "./controllers/order/CreateOrderController
 import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
 import { AddItemController } from "./controllers/order/AddItemController";
 import { RemoveItemController } from "./controllers/order/RemoveItemController";
+import { SendOrderController } from "./controllers/order/SendOrderController";
 
 const router = Router();
 
@@ -51,5 +52,6 @@ router.delete(
   isAuthenticated,
   new RemoveItemController().handle
 );
+router.put("/order/send", isAuthenticated, new SendOrderController().handle);
 
 export { router };
