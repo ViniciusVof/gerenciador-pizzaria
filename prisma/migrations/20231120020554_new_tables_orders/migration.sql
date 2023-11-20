6fc1d5c010f65a817c461fcd5a5c8be5528e30f3
Vinicius Freitas <viniciusfreitas@MacBook-Pro-de-MacInBox.local>
2023-11-19 23:06:08 -0300

    Nova tabela para pedidos

---
+++ prisma/migrations/20231120020554_new_tables_orders/migration.sql
@@ -0,0 +1,17 @@
+-- DropForeignKey
+ALTER TABLE "order_items" DROP CONSTRAINT "order_items_order_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "order_items" DROP CONSTRAINT "order_items_product_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "products" DROP CONSTRAINT "products_category_id_fkey";
+
+-- AddForeignKey
+ALTER TABLE "products" ADD CONSTRAINT "products_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "order_items" ADD CONSTRAINT "order_items_order_id_fkey" FOREIGN KEY ("order_id") REFERENCES "orders"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "order_items" ADD CONSTRAINT "order_items_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE CASCADE ON UPDATE CASCADE;
